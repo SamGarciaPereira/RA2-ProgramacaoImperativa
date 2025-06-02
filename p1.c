610d99b62168f22c9000da799e8b13e943856dd5
Samuel Garcia Pereira <samgarciapereira@gmail.com>
2025-06-02 16:03:22 -0300

    criação do arquivo csv e comentários sobre objetivos do programa 1

---
+++ p1.c
@@ -0,0 +1,7 @@
+/*1 - ler carros.csv (sequência: Marca, Modelo, Ano, Km, Preço)*/
+/*2 - armazenar esses dados em memória por meio de uma lista encadeada: cada nó da
+lista contém os dados de um carro, sendo que a ordem dos carros na lista deve ser
+a mesma ordem constante no arquivo carros.csv. */
+/*3 - salva em um arquivo binário denominado carros.bin a sequência de registros de
+carro constante na lista encadeada. Nesse arquivo, o primeiro dado a ser gravado
+(antes dos dados sobre carros) é a quantidade de registros sobre carros.*/
